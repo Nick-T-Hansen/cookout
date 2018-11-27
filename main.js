48e0dcee870cd76fc37be8f18bd42dd0dd66df1f
Nick Hansen <nick.hansen13@gmail.com>
2018-11-27 09:02:33 -0600

    code for exercise

---
+++ main.js
@@ -0,0 +1,46 @@
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An first array containing the objects to be cooked.
+const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+// This exercise does not require you to alter this function. But read it. See if you can follow what it takes in, executes, and returns.
+
+// The grill() function. Copy this into your JavaScript as well.
+
+function grill (currentObject) {
+    currentObject.cooked = true;
+    cookedFood.push(currentObject);
+    return(cookedFood);
+    
+}
+
+for (i = 0; i < uncookedFood.length; i++) {
+    grill (uncookedFood[i].name)
+}
+console.log(cookedFood)
